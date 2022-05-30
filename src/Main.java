bc091790d531eefd708b8609258ccd3a5d973f90
kuthurikavya <kuthurikavya1999@gmail.com>
2022-05-30 22:46:31 +0530

    first branch 'iss1' commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import com.cts.serives.Services;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Kavya");
 		System.out.println("Hope u r able to follow");
+		Services ss=new Services();
+		System.out.println(ss.findLenght("kavya reddy"));
 	}
 
 }
---
+++ src/com/cts/serives/Services.java
@@ -0,0 +1,8 @@
+package com.cts.serives;
+
+public class Services {
+	public int findLenght(String str) {
+		return str.length();
+	}
+
+}
